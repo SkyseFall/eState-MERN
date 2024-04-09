f641be9c84028163bebb59c52d62d1df840122f0
SkyseFall <aakashk1234a@gmail.com>
2024-04-09 20:36:10 +0530

    create SIGNIN route with JWT

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorhandle } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 
 export const signup = async (req, res, next) => {
@@ -17,3 +18,25 @@ export const signup = async (req, res, next) => {
         // next(errorhandle(550, "Error from This function")); // custom error handler.
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorhandle(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorhandle(401, "Wrong Credential"));
+
+        // destructure for password
+        const { password: pass, ...restInfo
+        } = validUser._doc;
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        res.cookie("access_token", token, { httpOnly: true })
+            .status(200)
+            .json(restInfo);
+    } catch (error) {
+        next(error);
+    }
+}
