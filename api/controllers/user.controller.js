5cbbbd16d0f0fd8ceb6b628873c3f097622ef68a
SkyseFall <aakashk1234a@gmail.com>
2024-04-14 09:14:02 +0530

    create User update API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,34 @@
 
 // logic here.
 
+import User from "../models/user.model.js";
+import { errorhandle } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
     // res.json({ message: "How are u ?" });
     res.send("Hello, Pintu");
+};
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorhandle(401, "You can only update your own account !"))
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+        const upadteUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, { new: true })  // add new: true will get the new response, otherwise get old data.
+
+        const { password, ...restInfo } = upadteUser._doc;
+        res.status(200).json(restInfo);
+    } catch (error) {
+        next(error);
+    }
 };
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { errorhandle } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+
+    if (!token) return next(errorhandle(401, "Unauthorized"));
+
+
+    jwt.verify(token, process.env.JWT_secret, (err, user) => {
+        if (err) {
+            return next(errorhandle(403, "Forbidden"));
+        }
+        req.user = user;
+        next();
+    });
+};
